3a2b1e3593bc465685a826ea4dc5f24adc8fd1d7
Ram <ramc@thoughtworks.com>
2011-03-14 16:42:49 +0530

    using dml instead of version_helper to create version table

--- db/migrate/20110314110935_create_wiki_version_table.rb
+++ db/migrate/20110314110935_create_wiki_version_table.rb
@@ -1,9 +1,23 @@
 class CreateWikiVersionTable < ActiveRecord::Migration
   def self.up
-    Wiki.create_versioned_table
+    create_table "wiki_versions", :force => true do |t|
+        t.integer  "wiki_id"
+        t.integer  "version"
+        t.string   "paadal"
+        t.string   "paadal_english"
+        t.string   "english_short_desc"
+        t.string   "tamil_short_desc"
+        t.text     "tamil_long_desc"
+        t.string   "state"
+        t.datetime "created_at"
+        t.datetime "updated_at"
+      end
+
+      add_index "wiki_versions", ["wiki_id"], :name => "index_wiki_versions_on_wiki_id"
+
   end
 
   def self.down
-    Wiki.drop_versioned_table
+    drop_table :wiki_versions
   end
 end
