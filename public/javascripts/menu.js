ebbd53c3d1d03aa2b88681f49bc7a89d6c8f78c5
Ram <ramc@thoughtworks.com>
2011-04-17 01:52:06 +0530

    beautify login page

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -7,4 +7,13 @@ $(document).ready(function(){
             show: 'fade'
         });
     })
+
+    $('#about_site').click(function(){
+        $('#about_site_content').dialog({
+            minWidth: 700,
+            maxHeight: 700,
+            modal: true,
+            show: 'fade'
+        });
+    })
 });
