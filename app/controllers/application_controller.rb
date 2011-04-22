e288e4def8da576e8894dce516726d69e18395cf
Ram <ramc@thoughtworks.com>
2011-04-22 18:50:21 +0530

    Extracted menu as a separate partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include TooltipHelper
+
+  def after_sign_in_path_for(resource)
+    p "-"*50
+    p resource
+    super
+  end
 end
--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
-    $('#mugavurai').click(function(){
+    $('#mugavurai').click(function(e){
         $('#mugavurai_content').dialog({
             minWidth: 700,
             maxHeight: 700,
             modal: true,
             show: 'fade'
         });
+        e.stopPropagation();
     });
 
-    $('#about_site').click(function(){
+    $('#about_site').click(function(e){
         $('#about_site_content').dialog({
             minWidth: 850,
             maxHeight: 700,
             modal: true,
             show: 'fade'
         });
+        e.stopPropagation();
     });
 
+    $('#user_login').click(function(e){
+        e.stopPropagation();
+    });
+
+    $('.logo').click(function(){
+        window.location = "/";
+    });
 });
