c4aa482f409da5070cf8454a046c948304e7c760
Ram <ramc@thoughtworks.com>
2011-04-25 00:07:30 +0530

    wiki diff of versions

---
+++ app/controllers/wiki_versions_controller.rb
@@ -0,0 +1,17 @@
+class WikiVersionsController < ApplicationController
+
+  def history
+    wiki = Wiki.find(params[:wiki_id])
+    @wiki_versions = wiki.versions.active
+    @wiki_versions << wiki.versions.archived.order('updated_at desc')
+    @wiki_versions.flatten!
+    render :layout => false
+  end
+
+  def diff
+    @current_wiki_version = WikiVersion.find(params[:id])
+    @previous_wiki_version = @current_wiki_version.previous_version
+#    render :layout => false
+  end
+
+end
