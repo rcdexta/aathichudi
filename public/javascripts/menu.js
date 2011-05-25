682b250efa712e6823ede69a2ee63080a50196cb
Ram <ramc@thoughtworks.com>
2011-05-25 20:41:17 +0530

    link to admin dashboard from menu

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,34 +1,31 @@
 $(document).ready(function(){
-    $('#mugavurai').click(function(e){
-        $('#mugavurai_content').dialog({
-            minWidth: 700,
-            maxHeight: 700,
-            modal: true,
-            show: 'fade'
-        });
-        e.stopPropagation();
+  $('#mugavurai').click(function(e){
+    $('#mugavurai_content').dialog({
+      minWidth: 700,
+      maxHeight: 700,
+      modal: true,
+      show: 'fade'
     });
+    e.stopPropagation();
+  });
 
-    $('#about_site').click(function(e){
-        $('#about_site_content').dialog({
-            minWidth: 850,
-            maxHeight: 700,
-            modal: true,
-            show: 'fade'
-        });
-        e.stopPropagation();
+  $('#about_site').click(function(e){
+    $('#about_site_content').dialog({
+      minWidth: 850,
+      maxHeight: 700,
+      modal: true,
+      show: 'fade'
     });
+    e.stopPropagation();
+  });
 
-    $('#user_login').click(function(e){
-        e.stopPropagation();
-    });
+  $('#user_login').click(function(e){
+    e.stopPropagation();
+  });
 
-    $('.logo').click(function(){
-        window.location = "/";
-    });
-
-    $('#home').click(function(){
-        window.location = "/";
-    });
+  $('.logo').click(function(){
+    window.location = "/";
+  });
 
 });
+
