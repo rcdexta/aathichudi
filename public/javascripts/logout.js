f72bb828b0b520a092231ee51d4423daa726be33
RC <rc.chandru@gmail.com>
2011-07-05 20:38:22 +0530

    final review changes for version1

--- public/javascripts/logout.js
+++ public/javascripts/logout.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-  $('#logout_link').click(function(){
-    var provider_logout_url = $("input[name='provider_signout_url']").val();
-    window.open(provider_logout_url);
+jQuery(document).ready(function(){
+  jQuery('#logout_link').click(function(){
+    var provider_logout_url = jQuery("input[name='provider_signout_url']").val();
+    var pop_up = window.open(provider_logout_url);
   });
 });
